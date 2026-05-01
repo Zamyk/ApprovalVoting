a591a357aca8f5078de4bf4d4d2d8a996dcf086b
zamyk <zamyk@hotmail.com>
2026-05-01 22:52:58 +0200

    some example approval elections

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from votekit.ballot import Ballot
 from votekit.ballot import ApprovalBallot
 
+from votekit.pref_profile.approval_profile import ApprovalProfile
+
+from votekit.elections.election_types.approval.hamming import OrderedWeightedHamming
+
 ballot = Ballot(approvals=["A", "B", "C"], weight=3 / 2)
 print(ballot)
+
+candidates = {"A", "B", "C", "D"}
+ballots = [
+  Ballot(approvals=["A", "B"]),
+  Ballot(approvals=["A", "B"]),
+  Ballot(approvals=["A", "D"]),
+  Ballot(approvals=["A", "B", "C", "D"])
+]
+
+profile = ApprovalProfile(ballots=ballots, candidates=candidates)
+print(profile)
+
+election = OrderedWeightedHamming(profile=profile)
+print(election)
